45f5a3192c46d004e1ed5d7cf4869fbfa2a7523d
Graphic <graphic@SOD-SJH-605-11M.local>
2016-02-09 16:51:55 -0500

    second

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+
+$('.hero').first().addClass('shown');
+
+$('h1.name').text($('.hero')
+	.first()
+	.addClass('shown')
+	.attr('title'));
+
+$('button.next').on('click', function(){
+
+$('.shown')
+.removeClass('shown')
+.next()
+.addClass('shown');
+
+});
+	
+$('button.prev').on('click', function(){
+
+
+
+if ($('.shown').is(':last-child')){
+
+$('.shown')
+.removeClass('shown')
+.siblings('.hero')
+.first()
+.addClass('shown');
+
+}
+
+else { $('.shown')
+.removeClass('shown')
+.prev()
+.addClass('shown');
+}
+
+
+var name = $('.shown').attr('.title');
+$('h1.name').text('.name');
