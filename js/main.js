883efb9cf462c9210343abd696eb3f51b8c58307
Graphic <graphic@s2-02ai.local>
2016-02-16 15:03:34 -0500

    final2

--- js/main.js
+++ js/main.js
@@ -1,43 +1,36 @@
-
-
 $('.hero').first().addClass('shown');
 
-$('h1.name').text($('.hero')
-	.first()
-	.addClass('shown')
-	.attr('title'));
+var name = $('.shown').attr('alt');
+$('h1.name').text(name);
 
 $('button.next').on('click', function(){
-
-$('.shown')
-.removeClass('shown')
-.next()
-.addClass('shown');
-
+	if(
+		$('.shown').is(':last-child')){
+			$('.shown').removeClass('shown');
+			$('.hero').first().addClass('shown')}
+	else	
+			{
+				$('.shown').removeClass('shown').next().addClass('shown');
+			};
+  
+var name = $('.shown').attr('alt');
+	$('h1.name').text(name);
 });
-	
+
+
 $('button.prev').on('click', function(){
+	if(
+		$('.shown').is(':first-child'))
+			{
+				$('.shown').removeClass('shown');
+				$('.hero').last().addClass('shown');
+			}
 
+	else	
+			{
+				$('.shown').removeClass('shown').prev().addClass('shown');
+			};
 
-
-if ($('.shown').is(':last-child')){
-
-$('.shown')
-.removeClass('shown')
-.siblings('.hero')
-.first()
-.addClass('shown');
-
-}
-
-else { $('.shown')
-.removeClass('shown')
-.prev()
-.addClass('shown');
-};
-
-
-var name = $('.shown').attr('title');
-$('h1').text(name);
-
+	var name = $('.shown').attr('alt');
+	$('h1.name').text(name);
 });
